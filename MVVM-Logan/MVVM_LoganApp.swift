cd3608ec4468c833f521fbdc6543c0f9ffb80953
Brian McIntosh <brian@mixinteractive.com>
2023-04-22 17:26:25 -0400

    Initial Commit

---
+++ MVVM-Logan/MVVM_LoganApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_LoganApp.swift
+//  MVVM-Logan
+//
+//  Created by Brian McIntosh on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_LoganApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
